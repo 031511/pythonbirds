1168e5695b797d3b3fc9f1d10ac8bfe95441ef1e
Vander <vanderguita@yahoo.com.br>
2020-12-28 09:53:35 -0300

    Criado atributo complexo Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Fabiano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Vander'
-    print(p.nome)
-    print(p.idade)
+    vander = Pessoa(nome= 'Vander')
+    fabiano = Pessoa(vander, nome='Fabiano')
+    print(Pessoa.cumprimentar(fabiano))
+    print(id(fabiano))
+    print(fabiano.cumprimentar())
+    print(fabiano.nome)
+    print(fabiano.idade)
+    for filho in fabiano.filhos:
+        print(filho.nome)
